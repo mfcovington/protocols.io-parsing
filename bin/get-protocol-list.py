34bdd4daee725ee7f060e6ae0e5d1f8b62502138
Mike Covington <mfcovington@gmail.com>
2017-10-14 17:42:20 -0700

    Get own protocol list

---
+++ bin/get-protocol-list.py
@@ -0,0 +1,17 @@
+import os
+import urllib.parse
+import urllib.request
+
+PROTOCOLS_IO_ACCESS_TOKEN = os.environ['PROTOCOLS_IO_ACCESS_TOKEN']
+
+url = 'https://protocols.io/api/open/get_protocols'
+values = {
+    'access_token': PROTOCOLS_IO_ACCESS_TOKEN,
+}
+
+data = urllib.parse.urlencode(values).encode("utf-8")
+req = urllib.request.Request(url, data)
+response = urllib.request.urlopen(req)
+the_page = response.read()
+
+print(the_page)
